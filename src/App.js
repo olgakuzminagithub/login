f85947dd890fa6a2407bfb1bdcf35883f4787302
olgakuzminagithub <olga.kuzmina1989@gmail.com>
2018-12-28 17:06:39 +0200

    add priject

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import "bootstrap/dist/css/bootstrap.css";
+
+import { LoginPage } from './features/login/LoginPage';
+
+
+export class App extends Component {
+
+    render() {
+        return (
+            <div className="container">
+                <div className="col-4 offset-4">
+                    <LoginPage/>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
